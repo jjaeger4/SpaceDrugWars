6986c1d0aa9abe856f8151636a936f6a6fc73317
Jesse Jaeger <jjaeger@rockislandauction.com>
2018-12-01 18:21:43 -0600

    Added a loans button

--- src/components/game/mainmenu.jsx
+++ src/components/game/mainmenu.jsx
@@ -15,6 +15,7 @@ export default class MainMenu extends React.Component {
         <div id="gameinfo">
           <div>Day: {appStore.day} of {appStore.lastday}</div>
           <div>Credits: {appStore.credits}</div>
+          <div>Loans: {appStore.loan} <button className="btn is-primary loans">Loans</button></div>
           <div>Cargo: {`${appStore.cargo} / ${appStore.cargoMax}`}</div>
         </div>
       </div>
